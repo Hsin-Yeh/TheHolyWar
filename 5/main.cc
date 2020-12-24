ef424896a94217984acd6bb6ce7f3ce1a7188dc6
HsinYeh <thankyouyou06@gmail.com>
2020-12-24 11:29:56 +0800

    blessings

---
+++ 5/main.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string curseWords(int i);
+string blessings(int i);
+int Get_some_Chocolate();
+int Pour_a_glass_of_wine();
+int Prepare_Christmas_gifts();
+bool Trial(int a, int b, int c);
+
+string curseWords(int i){
+  string s;
+  switch(i) {
+    case 0:
+      s = "The rain in Taipei will last for one month";
+      break;
+    case 1:
+      s = "Hahaha, you will take 10 years to graduate";
+      break;
+    case 2:
+      s = "Meetings will NOT be cancelled tommorrow! \nNo Christmas for you!!\nWork!!!!!";
+  }
+  return s;
+}
+
+string blessings(int i){
+  string s;
+  switch(i) {
+    case 0:
+      s = "It will be a sunny day tomorrow!";
+      break;
+    case 1:
+      s = "Congratulations to Page and Lao!";
+      break;
+    case 2:
+      s = "No meetings tomorrow~~~~~~~~~~~ Happy Christmas!";
+  }
+  return s;
+}
+
+int Get_some_Chocolate(){
+  return 0;
+}
+
+int Pour_a_glass_of_wine(){
+  return 0;
+}
+
+int Prepare_Christmas_gifts(){
+  return 0;
+}
+
+bool Trial(int a, int b, int c){
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+  bool theLittleXmas = 0; // 0 == devil littleXmas; 1 == angel littleXmas;
+
+  int a = Get_some_Chocolate();
+  int b = Pour_a_glass_of_wine();
+  int c = Prepare_Christmas_gifts();
+
+  theLittleXmas = Trial(a, b, c);
+
+  if ( theLittleXmas == 0 ) {
+    srand((unsigned) time(0));
+    cout << curseWords(rand()%3) << endl;
+  }
+  else {
+    srand((unsigned) time(0));
+    cout << blessings(rand()%3) << endl;
+  }
+
+  return 0;
+}
